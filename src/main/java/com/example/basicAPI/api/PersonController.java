7fd0a52b9a9e0da9b8b77edcff2cc9bfa2ae6088
santanasara <saraa.beatrizz@gmail.com>
2020-02-11 15:06:23 -0300

    First commit

---
+++ src/main/java/com/example/basicAPI/api/PersonController.java
@@ -0,0 +1,35 @@
+package com.example.basicAPI.api;
+
+import com.example.basicAPI.model.Person;
+import com.example.basicAPI.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
+@RestController
+public class PersonController {
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(@RequestBody Person person){
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id){
+        return personService.getPersonById(id).orElse(null);
+    }
+}
---
+++ src/test/java/com/example/basicAPI/BasicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
