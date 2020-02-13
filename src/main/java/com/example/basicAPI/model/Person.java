f409f05a4fa30448f45c5392aa8776988ad7b382
santanasara <saraa.beatrizz@gmail.com>
2020-02-13 12:31:40 -0300

    Added Lombok to the Person model class

--- src/main/java/com/example/basicAPI/model/Person.java
+++ src/main/java/com/example/basicAPI/model/Person.java
@@ -2,24 +2,19 @@ package com.example.basicAPI.model;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.UUID;
 
+@Getter
+@AllArgsConstructor
 public class Person {
+    @JsonProperty
     private final UUID id;
+    @JsonProperty
     private final String name;
 
-    public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public UUID getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
 
